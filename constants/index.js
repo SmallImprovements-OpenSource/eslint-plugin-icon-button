cea86dd4c1534b0b206e470e13dfe25ca52c211c
Charisse <charisse.ysabel930@gmail.com>
2018-11-16 10:41:51 +0100

    refactor: Remove unused functions

--- constants/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    BUTTON_SVG: 'icon buttons need aria-label attribute',
-    BUTTON_NO_TEXT: 'button should have text',
-    NO_STUPID_ARIA_LABELS: 'Aria labels should be descriptive',
-};
